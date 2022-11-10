a6e7ead8c48febbc83e73ecb275e226b6731bfc7
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-10 09:13:38 +0200

    18-challenge

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+    <div>
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px"/>
+            </nav>
+        </header>
+    </div>
+}
+
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px"/>
-                </nav>
-            </header>
+            <Header />
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
@@ -16,7 +28,9 @@ function Page() {
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
-            <footer>© 20xx Ivanov development. All rights reserved.</footer>
+            <footer>
+               <small> © 20xx Ivanov development. All rights reserved. </small>
+            </footer>
         </div>
     )
 }
