02d8e07e185225fc5924ebc13c8e46ae24548a07
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-10 08:41:07 +0200

    16.1

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import React from "react"
-import ReactDOM, { render } from "react-dom"
+import ReactDOM from "react-dom"
 
 function Page() {
     return (
         <div>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px"/>
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
                 able to fit in with the cool kids!</li>
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
+            <footer>© 20xx Ivanov development. All rights reserved.</footer>
         </div>
     )
 }
