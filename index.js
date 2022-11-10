f7818762dc2da76a9e66f35995bd7a2b689b1630
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-10 10:24:05 +0200

    19-styling

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 /**
-Mini Challenge:
-Move the `header` element from Page into 
-its own component called "Header"
+Challenge: 
+
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+  - Using flexbox, line up the nav items horizontally, and
+  put them inline with the React logo.
+  - Change the image styling to happen in CSS instead of in-line
+  For practice, add a new class to the image in order to style it
 */
 
 function Header() {
     return (
         <header>
-            <nav>
-                <img src="./react-logo.png" width="40px"/>
+            <nav className="nav">
+                <img className="image" src="./react-logo.png" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     )
