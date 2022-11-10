25b57a7d574f540cd7671c351f921edb9c04e489
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-10 14:30:15 +0200

    23

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer>
-            <small> © 20xx Ivanov development. All rights reserved. </small>
-        </footer>
-    )
-}
--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Header from "./Header"
-import Footer from "./Footer"
-import MainContent from "./MainContent"
+/**
+Challenge: Project setup
 
-/**
-/**
-Challenge: move the Footer and MainContent components
-into their own files.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
 */
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
