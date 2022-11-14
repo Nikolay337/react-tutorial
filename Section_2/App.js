14afa1618aa5b91aadac2a511ff68026ff2233f6
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-14 14:40:06 +0200

    Section_2-13

--- Section_2/App.js
+++ Section_2/App.js
@@ -1,37 +1,29 @@
 import React from "react"
-import Contact from "./Contact"
+import Joke from "./Joke"
 
-function App() {
-    return (
-      <div>
-        <div className="contacts">
-          <Contact 
-            img="./images/mr-whiskerson.png"
-            name="Mr. Whiskerson"
-            phone="(212) 555-1234"
-            email="mr.whiskaz@catnap.meow"
-          />
-          <Contact 
-            img="./images/fluffykins.png"
-            name="Fluffykins"
-            phone="(212) 555-2345"
-            email="fluff@me.com"
-          />
-          <Contact 
-            img="./images/felix.png"
-            name="Felix"
-            phone="(212) 555-4567"
-            email="thecat@hotmail.com"
-          />
-          <Contact 
-            img="./images/pumpkin.png"
-            name="Pumpkin"
-            phone="(0800) CAT KING"
-            email="pumpkin@scrimba.com"
-          />
-        </div>
-      </div>
-    )
+export default function App() {
+  return (
+    <div>
+      <Joke 
+        setup="I got my daughter a fridge for her birthday." 
+        punchline="I can't wait to see her face light up when she opens it." 
+      />
+      <Joke 
+        setup="How did the hacker escape the police?" 
+        punchline="He just ransomware!" 
+      />
+      <Joke 
+        setup="Why don't pirates travel on mountain roads?" 
+        punchline="Scurvy." 
+      />
+      <Joke 
+        setup="Why do bees stay in the hive in the winter?" 
+        punchline="Swarm." 
+      />
+      <Joke 
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!" 
+      />
+    </div>
+  )
 }
-
-export default App
