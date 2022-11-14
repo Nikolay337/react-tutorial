008bd0bc6f9b329176685949b44b2b1fb7905b4a
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-14 20:34:23 +0200

    Section_2-15

--- Section_2/App.js
+++ Section_2/App.js
@@ -1,28 +1,20 @@
-import React from "react"
-import Joke from "./Joke"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import Hero from "./components/Hero"
+import Katie from "../src/images/katie-zaferes.png"
 
 export default function App() {
+  //<Hero />
   return (
     <div>
-      <Joke 
-        setup="I got my daughter a fridge for her birthday." 
-        punchline="I can't wait to see her face light up when she opens it." 
-      />
-      <Joke 
-        setup="How did the hacker escape the police?" 
-        punchline="He just ransomware!" 
-      />
-      <Joke 
-        setup="Why don't pirates travel on mountain roads?" 
-        punchline="Scurvy." 
-      />
-      <Joke 
-        setup="Why do bees stay in the hive in the winter?" 
-        punchline="Swarm." 
-      />
-      <Joke 
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!" 
+      <Navbar />
+      <Card
+        img={Katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USE"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
       />
     </div>
   )
