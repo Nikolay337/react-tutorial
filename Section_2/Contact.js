61a36b97b33968a7108d212dc3aacff9d46e8d4f
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-14 06:38:53 +0200

    Section_2-10

--- Section_2/Contact.js
+++ Section_2/Contact.js
@@ -1,27 +1,17 @@
 import React from "react"
 
-/* Challenge:
-
-- Create a Contact.js component in another file
-- Move one of the contact card divs below into that file
-- import and render 4 instances of that contact card
-    - Think ahead: what's the problem with doing it this way?
-*/
-
-function Contact() {
+function Contact(props) {
   return (
-    <div className="contacts">
-      <div className="contact-card">
-        <img src="./images/mr-whiskerson.png"/>
-        <h3>Mr. Whiskerson</h3>
-        <div className="info-group">
-          <img src="./images/phone-icon.png" />
-          <p>(212) 555-1234</p>
-        </div>
-        <div className="info-group">
-          <img src="./images/mail-icon.png" />
-          <p>mr.whiskaz@catnap.meow</p>
-        </div>
+    <div className="contact-card" >
+      <img src={props.img}/>
+      <h3>{props.name}</h3>
+      <div className="info-group">
+        <img src="./images/phone-icon.png" />
+        <p>{props.phone}</p>
+      </div>
+      <div className="info-group">
+        <img src="./images/mail-icon.png" />
+        <p>{props.email}</p>
       </div>
     </div>
   )
