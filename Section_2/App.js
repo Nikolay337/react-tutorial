a1631864cff2b252ee1171f08206b31d87fbfb5e
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2022-11-16 09:10:53 +0200

    Section_2-21

--- Section_2/App.js
+++ Section_2/App.js
@@ -1,14 +1,39 @@
-import Joke from "./components/Joke"
-import jokesData from "./components/jokesData"
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
 
 export default function App() {
-  const jokeElement = jokesData.map(joke => {
-    return <Joke setup={joke.setup} punchline={joke.punchline} />
-  })
-
-  return (
-    <div>
-      {jokeElement}
-    </div>
-  )
+            // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            {cards}
+        </div>
+    )
 }
